00c78227af6d6f03970f863314a0935ca5585995
Lukas Maruniak <lukas.maruniak@d-wise.com>
2023-05-01 21:19:53 +0200

    Added computation by matrix

--- main.cpp
+++ main.cpp
@@ -59,6 +59,55 @@ private:
     }
 };
 
+class GraphM {
+public:
+    GraphM(int n) : adj_matrix(n, vector<int>(n)) {}
+
+    void add_edge(int u, int v) {
+        adj_matrix[u][v] = 1;
+        adj_matrix[v][u] = 1;
+    }
+
+    int num_vertices() {
+        return adj_matrix.size();
+    }
+
+    int num_edges() {
+        int num_edges = 0;
+        int n = num_vertices();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (adj_matrix[i][j] == 1) {
+                    num_edges++;
+                }
+            }
+        }
+        return num_edges;
+    }
+
+    double global_clustering_coefficient() {
+        int n = num_vertices();
+        int num_triangles = 0;
+        int num_triplets = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (adj_matrix[i][j] == 1) {
+                    for (int k = j + 1; k < n; k++) {
+                        if (adj_matrix[i][k] == 1 && adj_matrix[j][k] == 1) {
+                            num_triangles++;
+                        }
+                    }
+                    num_triplets++;
+                }
+            }
+        }
+        return (double) 2 * num_triangles / (double) (num_triplets * (num_triplets - 1));
+    }
+
+private:
+    vector<vector<int>> adj_matrix;
+};
+
 ifstream read_file() {
     string filepath;
     cout << "Enter the file path: ";
@@ -100,15 +149,21 @@ int main() {
 //    print_set(vertices);
 
     Graph g(num_vertices);
+    GraphM gm(num_vertices);
 
     for (auto it = begin(edges); it != end(edges); ++it) {
         g.add_edge(it->node1, it->node2);
         g.add_edge(it->node2, it->node1);
+
+        gm.add_edge(it->node1, it->node2);
+        gm.add_edge(it->node2, it->node1);
     }
 
     double global_cc = g.global_clustering_coefficient();
+    double global_m_cc = gm.global_clustering_coefficient();
 
     cout << "Global clustering coefficient: " << global_cc << endl;
+    cout << "Global clustering coefficient[Matrix]: " << global_m_cc << endl;
 
     return 0;
 }
